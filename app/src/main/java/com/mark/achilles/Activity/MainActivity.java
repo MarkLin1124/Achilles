762f69108a1b8830daff23e52af94590700cd182
林合軒 <marklin1124@gmail.com>
2017-12-26 14:23:22 +0800

    MainActivity UI finish

--- app/src/main/java/com/mark/achilles/Activity/MainActivity.java
+++ app/src/main/java/com/mark/achilles/Activity/MainActivity.java
@@ -3,9 +3,15 @@ package com.mark.achilles.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mark.achilles.Module.BoxScore;
+import com.mark.achilles.Module.GameInfo;
 import com.mark.achilles.R;
 
 public class MainActivity extends BaseActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private BoxScore mBoxScore;
+    private GameInfo mGameInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
